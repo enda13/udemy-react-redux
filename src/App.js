748d93a164f4c7acc7d4e501f21df22767d3fd4d
enda13 <satoshi.sonoda94@gmail.com>
2021-01-03 00:50:16 +0900

    about props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
 function App() {
+  const profiles = [
+    {name: "Tom", age: 10},
+    {name: "Mary", age: 15}
+  ]
   return (
     <div>
-        <p>
-          Hello world!
-        </p>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index}></User>
+      })}
     </div>
   );
+}
+
+function User(props) {
+  return (
+    <div>
+      Hi, I'm {props.name} and {props.age} years old!
+    </div>
+  )
+}
+
+User.defaulProps = {
+  age: 21
 }
 
 export default App;
